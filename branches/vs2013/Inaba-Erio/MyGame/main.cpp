d955c2cd425cbc5663eb5ca384034f79096ece8c
nacho692@gmail.com <nacho692@gmail.com@789ea820-3a0d-8c3e-127e-ea0f46427b86>
2014-04-01 23:57:34 +0000

    Codigo mas limpito

--- branches/vs2013/Inaba-Erio/MyGame/main.cpp
+++ branches/vs2013/Inaba-Erio/MyGame/main.cpp
@@ -2,7 +2,6 @@
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	Window w(hInstance,nCmdShow);
+	Window w(nCmdShow);
 	w.Create(640,480);
-	w.GetMsg();
 }
