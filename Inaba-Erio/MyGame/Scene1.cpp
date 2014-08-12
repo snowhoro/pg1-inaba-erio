2af508f95e3c95b34755888a37c74d6715093f55
Snow.xs@gmail.com <Snow.xs@gmail.com@789ea820-3a0d-8c3e-127e-ea0f46427b86>
2014-08-12 21:25:06 +0000

    Comments

--- Inaba-Erio/MyGame/Scene1.cpp
+++ Inaba-Erio/MyGame/Scene1.cpp
@@ -23,7 +23,7 @@ Scene1::~Scene1()
 
 	delete _sprite1;
 	_sprite1 = NULL;
-	
+
 	delete _sprite2;
 	_sprite2 = NULL;
 }
@@ -37,62 +37,63 @@ bool Scene1::Init(Inaba::Renderer& renderer)
 	return true;
 }
 bool Scene1::Frame(Inaba::Renderer& renderer, Inaba::DirectInput& directInput, Inaba::Timer& timer)
-{	
-	 static float fSp = 1.0f;
-	 static float vSpd = 10.0f;
-	 std::string number;
-	 std::stringstream strstream;
-	 strstream <<directInput.mouseRelPosX();
-	 strstream >> number;
+{
+	static float fSp = 1.0f;
+	static float vSpd = 10.0f;
+	std::string number;
+	std::stringstream strstream;
+	strstream << directInput.mouseRelPosX();
+	strstream >> number;
+	renderer.getFont()->Print((char*)("mouse X: " + number).c_str(), 0, 0, D3DCOLOR_XRGB(255, 255, 255), NULL, 200, 0, Inaba::FA_LEFT);
+	
+	// ------------------------------------------------
+	//				CAMERA CONTROL
+	// ------------------------------------------------
+	if (directInput.keyDown(Inaba::Input::KEY_W))
+		renderer.getCamera()->MoveForward(5.0f);
+	if (directInput.keyDown(Inaba::Input::KEY_S))
+		renderer.getCamera()->MoveForward(-5.0f);
+	if (directInput.keyDown(Inaba::Input::KEY_A))
+		renderer.getCamera()->Strafe(-5.0f);
+	if (directInput.keyDown(Inaba::Input::KEY_D))
+		renderer.getCamera()->Strafe(5.0f);
+	if (directInput.keyDown(Inaba::Input::KEY_Q))
+		renderer.getCamera()->MoveUp(-5.0f);
+	if (directInput.keyDown(Inaba::Input::KEY_E))
+		renderer.getCamera()->MoveUp(5.0f);
 
-	 renderer.getFont()->Print((char*)("mouse X: " + number).c_str(), 0, 0, D3DCOLOR_XRGB(255, 255, 255), NULL, 200, 0, Inaba::FA_LEFT);
-
-	 // ------------------------------------------------
-	 //				CAMERA CONTROL
-	 // ------------------------------------------------
-	 if (directInput.keyDown(Inaba::Input::KEY_W))
-		 renderer.getCamera()->MoveForward(5.0f);
-	 if (directInput.keyDown(Inaba::Input::KEY_S))
-		 renderer.getCamera()->MoveForward(-5.0f);
-	 if (directInput.keyDown(Inaba::Input::KEY_A))
-		 renderer.getCamera()->Strafe(-5.0f);
-	 if (directInput.keyDown(Inaba::Input::KEY_D))
-		 renderer.getCamera()->Strafe(5.0f);
-	 if (directInput.keyDown(Inaba::Input::KEY_Q))
-		 renderer.getCamera()->MoveUp(-5.0f);
-	 if (directInput.keyDown(Inaba::Input::KEY_E))
-		 renderer.getCamera()->MoveUp(5.0f);
-
-	 if (directInput.keyDown(Inaba::Input::KEY_LCONTROL)){
-		 renderer.getCamera()->Yaw(D3DXToRadian(directInput.mouseRelPosX()));
-		 renderer.getCamera()->Pitch(D3DXToRadian(directInput.mouseRelPosY()));
-	 }
-	 // ------------------------------------------------
-
-	 if (directInput.keyDown(Inaba::Input::KEY_SUBTRACT))
-		 _sprite1->setPos(_sprite1->posX(), _sprite1->posY(), _sprite1->posZ() - vSpd);
-
-	 if (directInput.keyDown(Inaba::Input::KEY_ADD))
-		 _sprite1->setPos(_sprite1->posX(), _sprite1->posY(), _sprite1->posZ() + vSpd);
+	if (directInput.keyDown(Inaba::Input::KEY_LCONTROL)){
+		renderer.getCamera()->Yaw(D3DXToRadian(directInput.mouseRelPosX()));
+		renderer.getCamera()->Pitch(D3DXToRadian(directInput.mouseRelPosY()));
+	}
+	// ------------------------------------------------
 
 
-	 if (directInput.keyDown(Inaba::Input::KEY_UP))
-		 _sprite1->setPos(_sprite1->posX(), _sprite1->posY() + fSp);
-	 else if(directInput.keyDown(Inaba::Input::KEY_DOWN))
-	  _sprite1->setPos(_sprite1->posX(), _sprite1->posY() - fSp);
-	 else if(directInput.keyDown(Inaba::Input::KEY_LEFT))
-	  _sprite1->setPos(_sprite1->posX() - fSp, _sprite1->posY());
-	 else if(directInput.keyDown(Inaba::Input::KEY_RIGHT))
-	  _sprite1->setPos(_sprite1->posX() + fSp, _sprite1->posY());
+	// ------------------------------------------------
+	//				SPRITE CONTROL
+	// ------------------------------------------------
+	if (directInput.keyDown(Inaba::Input::KEY_SUBTRACT))
+		_sprite1->setPos(_sprite1->posX(), _sprite1->posY(), _sprite1->posZ() - vSpd);
+	if (directInput.keyDown(Inaba::Input::KEY_ADD))
+		_sprite1->setPos(_sprite1->posX(), _sprite1->posY(), _sprite1->posZ() + vSpd);
 
+	if (directInput.keyDown(Inaba::Input::KEY_UP))
+		_sprite1->setPos(_sprite1->posX(), _sprite1->posY() + fSp);
+	else if (directInput.keyDown(Inaba::Input::KEY_DOWN))
+		_sprite1->setPos(_sprite1->posX(), _sprite1->posY() - fSp);
+	else if (directInput.keyDown(Inaba::Input::KEY_LEFT))
+		_sprite1->setPos(_sprite1->posX() - fSp, _sprite1->posY());
+	else if (directInput.keyDown(Inaba::Input::KEY_RIGHT))
+		_sprite1->setPos(_sprite1->posX() + fSp, _sprite1->posY());
+	// ------------------------------------------------
 
-	 //COLISION
-	if(_sprite1->checkCollision(*_quad1) != Inaba::Entity2D::NoCollision )
+	//COLISION
+	if (_sprite1->checkCollision(*_quad1) != Inaba::Entity2D::NoCollision)
 		_sprite1->returnToPos(_sprite1->prevPosX(), _sprite1->prevPosY());
-	if(_sprite1->checkCollision(*_quad2) != Inaba::Entity2D::NoCollision )
+	if (_sprite1->checkCollision(*_quad2) != Inaba::Entity2D::NoCollision)
 		_sprite1->returnToPos(_sprite1->prevPosX(), _sprite1->prevPosY());
 
-	if(directInput.keyDown(Inaba::Input::KEY_P))
+	if (directInput.keyDown(Inaba::Input::KEY_P))
 		_game->setScene("scene2", &renderer);
 
 	return true;
