ed7959cebba3960266a5387d7bc0c6d0bf4d2871
daniel.mont93@gmail.com <daniel.mont93@gmail.com@789ea820-3a0d-8c3e-127e-ea0f46427b86>
2014-09-01 01:21:25 +0000

    Mesh

---
+++ Inaba-Erio/MyEngine/Entity3D/Mesh.h
@@ -0,0 +1,33 @@
+#ifndef MESH_H
+#define MESH_H
+
+#include "Entity3D.h"
+#include "../Renderer/xMath.h"
+#include "../Renderer/RenderTypes.h"
+#include "../VertexBuffer/indexbuffer.h"
+#include "../VertexBuffer/vertexbuffer3D.h"
+#include "../myengine_api.h"
+
+namespace Inaba
+{
+	class Renderer;
+	class Timer;
+	
+	class MYENGINE_API Mesh : public Entity3D
+	{
+		public:
+			Mesh(Renderer & p_Renderer);
+			~Mesh();
+
+			void setData(size_t vertexCount, Inaba::Primitive, const unsigned short*, size_t indexCount);
+            void Draw(Renderer& r) const;
+		private:
+			ColorVertex* _vertex;
+			IndexBuffer* _indexBuffer;
+			VertexBuffer3D* _vertexBuffer3D;
+			Primitive pPrimitive;
+			Renderer& _renderer;
+	};
+}
+
+#endif
